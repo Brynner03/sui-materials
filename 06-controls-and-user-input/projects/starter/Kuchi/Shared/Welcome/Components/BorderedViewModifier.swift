c716163e25370e8d5c9c4616c31f7187a7591d45
Brynner03 <bventura03@hotmail.com>
2023-07-22 22:41:07 -0400

    Completed Chapter 6: Added User TextField, Remember Me option, I also learnt about reacting to user input, and when to use @EnvironmentalObject attribute.

--- 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/Components/BorderedViewModifier.swift
+++ 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/Components/BorderedViewModifier.swift
@@ -32,24 +32,29 @@
 
 import SwiftUI
 
-@main
-struct KuchiApp: App {
-    
-    let userManager = UserManager()
-    
-    init() {
-        userManager.load()
+struct BorderedViewModifier: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .padding(
+                EdgeInsets(
+                    top: 8, leading: 16, bottom: 8, trailing: 16))
+            .background(Color.white)
+            .overlay(
+                RoundedRectangle(cornerRadius: 8)
+                    .stroke(lineWidth: 2)
+                    .foregroundColor(.blue)
+            )
+            .shadow(
+                color: Color.gray.opacity(0.4),
+                radius: 3, x: 1, y: 2)
     }
-  var body: some Scene {
-    WindowGroup {
-      RegisterView()
-            .environmentObject(userManager)
-    }
-  }
 }
 
-struct KuchiApp_Previews: PreviewProvider {
-  static var previews: some View {
-    RegisterView()
-  }
+extension View {
+    func bordered() -> some View {
+        ModifiedContent(
+            content: self,
+            modifier: BorderedViewModifier()
+        )
+    }
 }
--- 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
+++ 06-controls-and-user-input/projects/starter/Kuchi/Shared/Welcome/RegisterView.swift
@@ -34,25 +34,49 @@ import SwiftUI
 
 struct RegisterView: View {
     
-    @State var name: String = ""
+    @EnvironmentObject var userManager: UserManager
+    @FocusState var nameFieldFocused: Bool
     
     var body: some View {
         VStack {
             Spacer()
             WelcomeMessageView()
-            TextField("Type your name...", text: $name)
-                .padding(
-                    EdgeInsets(
-                        top: 8, leading: 16, bottom: 8, trailing: 16))
-                .background(Color.white)
-                .overlay(
-                    RoundedRectangle(cornerRadius: 8)
-                        .stroke(lineWidth: 2)
-                        .foregroundColor(.blue)
-                )
-                .shadow(
-                    color: Color.gray.opacity(0.4),
-                    radius: 3, x: 1, y: 2)
+            TextField("Type your name...", text: $userManager.profile.name)
+                .focused($nameFieldFocused)
+                .submitLabel(.done)
+                .onSubmit(registerUser)
+                .bordered()
+            HStack {
+                Spacer()
+                Text("\(userManager.profile.name.count)")
+                    .font(.caption)
+                    .foregroundColor(
+                        userManager.isUserNameValid() ? .green : .red
+                    )
+                    .padding(.trailing)
+            }
+            .padding(.bottom)
+            HStack {
+                Spacer()
+                Toggle(isOn: $userManager.settings.rememberUser) {
+                    Text("Remember me")
+                        .font(.subheadline)
+                        .foregroundColor(.gray)
+                }
+                .fixedSize()
+            }
+            Button(action: registerUser) {
+                HStack {
+                    Image(systemName: "checkmark")
+                        .resizable()
+                        .frame(width: 16, height: 16, alignment: .center)
+                    Text("OK")
+                        .font(.body)
+                        .bold()
+                        .disabled(!userManager.isUserNameValid())
+                }
+            }
+            .bordered()
             Spacer()
         }
         .padding()
@@ -61,7 +85,26 @@ struct RegisterView: View {
 }
 
 struct RegisterView_Previews: PreviewProvider {
+    static let user = UserManager(name: "Brynner")
+    
     static var previews: some View {
         RegisterView()
+            .environmentObject(user)
+    }
+}
+
+// MARK: - Event Handlers
+extension RegisterView {
+    func registerUser() {
+        
+        nameFieldFocused = false
+        
+        if userManager.settings.rememberUser {
+            userManager.persistProfile()
+        } else {
+            userManager.clear()
+        }
+        userManager.persistSettings()
+        userManager.setRegistered()
     }
 }
